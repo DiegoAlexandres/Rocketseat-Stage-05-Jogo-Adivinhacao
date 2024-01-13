4bf015da43d03c3c68f69f4f44489a4f2dd5a940
DiegoAleandres <diegoalexandre.me@gmail.com>
2024-01-13 12:02:10 -0300

    Logica do projeto finalizada

--- main.js
+++ main.js
@@ -4,27 +4,51 @@ const screen2 = document.querySelector(".screen-2")
 const btnTry = document.querySelector("#btnTry")
 const btnReset = document.querySelector("#btnReset")
 
-let xAttempts = 5
+let xAttempts = 1
+
+function generateRandomNumber(){
+    return Math.round(Math.random() * 10)
+}
+
+randomNumber = generateRandomNumber()
+
+console.log(randomNumber)
 
 btnTry.addEventListener("click", whenClick)
 btnReset.addEventListener("click", hundleResetClick)
+document.addEventListener('keydown', function(e) {
+    if(e.key == "Enter" && screen1.classList.contains("hide")) {
+        hundleResetClick()
+    }
+
+})
+
 
 function whenClick(event){
     event.preventDefault()
 
     const inputNumber = document.querySelector("#inputNumber")
 
-    if(Number(inputNumber.value) == xAttempts){
+    if(Number(inputNumber.value) == randomNumber){
         screen1.classList.add("hide")
         screen2.classList.remove("hide")
+
+        document.querySelector(".screen-2 h2").innerText = `Acertou em ${xAttempts} tentativas!`
     }
    
+    inputNumber.value = ''
+
+    xAttempts++
 }
 
-function hundleResetClick(event){
-    event.preventDefault()
-
+function hundleResetClick(){
     screen1.classList.remove("hide")
     screen2.classList.add("hide")
 
+    randomNumber = generateRandomNumber()
+    xAttempts = 1
+
+    console.log(randomNumber)
 }
+
+
