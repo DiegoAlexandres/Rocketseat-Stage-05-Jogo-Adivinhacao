b8d2673d7e3f8554535d2b8bd2d4c4fea7d6a647
DiegoAleandres <diegoalexandre.me@gmail.com>
2024-01-13 10:50:59 -0300

    Desenvolvimento das interaões da tela 1 para a tela 2 com javascript

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
-const screen1 = document.querySelector(".screen-01")
-const screen2 = document.querySelector(".screen-02")
+const screen1 = document.querySelector(".screen-1")
+const screen2 = document.querySelector(".screen-2")
 
-function whenClick (event){
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+
+let xAttempts = 5
+
+btnTry.addEventListener("click", whenClick)
+btnReset.addEventListener("click", hundleResetClick)
+
+function whenClick(event){
     event.preventDefault()
 
-    screen1.classList.toggle("hide")
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if(Number(inputNumber.value) == xAttempts){
+        screen1.classList.add("hide")
+        screen2.classList.remove("hide")
+    }
+   
+}
+
+function hundleResetClick(event){
+    event.preventDefault()
+
+    screen1.classList.remove("hide")
+    screen2.classList.add("hide")
+
 }
